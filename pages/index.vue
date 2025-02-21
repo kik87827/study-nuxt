8dcc34b43a728aaf36bb32e542bb3ef3c2269187
kik <kik87827@gmail.com>
2025-02-21 15:34:44 +0900

    스터디 진행중

---
+++ pages/index.vue
@@ -0,0 +1,12 @@
+<template>
+  <div>index</div>
+</template>
+
+<script>
+export default {
+  name: "App",
+  components: {},
+};
+</script>
+
+<style scoped></style>
---
+++ pages/main.vue
@@ -0,0 +1,14 @@
+<template>
+    <div><h1>메인 페이지</h1></div>
+</template>
+
+<script>
+export default {
+name : 'Main',
+components : {
+}
+}
+</script>
+
+<style scoped>
+</style>
